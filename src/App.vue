7fc70463ac092f58fbe9d6ebce56384a053e8f75
jtsillan <juha.sillanpaa@edu.lapinamk.fi>
2022-03-16 11:46:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import PublicationViewAll from './components/publication/PublicationViewAll.vue';
+</script>
+
+<template>
+<PublicationViewAll></PublicationViewAll>
+</template>
+
+<style>
+  
+</style>
