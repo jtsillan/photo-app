55166c41430536fe897b089f8b35adae5de777ee
jtsillan <juha.sillanpaa@edu.lapinamk.fi>
2022-03-21 13:37:50 +0200

    add fetch and publication visualis

--- src/components/publication/PublicationViewAll.vue
+++ src/components/publication/PublicationViewAll.vue
@@ -1,11 +1,55 @@
 <script setup>
 
+import { ref } from 'vue';
+
+
+    const publications = ref([])
+
+    const getAllPublications = async () => {
+
+        const responce = await fetch('https://juhaniguru-web-nosql-python.onrender.com/api/publications')
+        const data = await responce.json()
+
+        publications.value = data.publications
+    }
+    
+    getAllPublications()
+
 </script>
 
 <template>
-    <h1>Moikka</h1>
+
+<div class="container">
+  <div class="item" v-for="publication in publications">
+        <!-- {{ publication.url }} -->
+
+        {{ publication.title }}
+        <img :src="publication.url">
+    </div>
+</div>
+    
+  
+
 </template>
 
-<style>
+<style scoped>
+.item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: rgb(197, 194, 194);
+    margin: 20px;
+}
+
+img {
+    width: 400px;
+    margin: 20px;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
 
 </style>
